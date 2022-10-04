f0a6085a34ecd66c894ff976490916580f250c77
Medet <medetnurm@gmail.com>
2022-10-04 16:07:23 +0600

    program of task

---
+++ zadacha/Program.cs
@@ -0,0 +1,33 @@
+﻿/*Написать программу, которая из имеющегося массива строк формирует 
+новый массив из строк, длина которых меньше, либо равна 3 символам. 
+Первоначальный массив можно ввести с клавиатуры, либо задать на старте выполнения алгоритма. 
+При решении не рекомендуется пользоваться коллекциями, лучше обойтись исключительно массивами.
+Примеры:
+[“Hello”, “2”, “world”, “:-)”] → [“2”, “:-)”]
+[“1234”, “1567”, “-2”, “computer science”] → [“-2”]
+[“Russia”, “Denmark”, “Kazan”] → []*/
+
+string[] arrayFull = new string[4] {"home", "756", "4567", "eat"};
+string[] arrayNew = new string[arrayFull.Length];
+void ArrayLessEqualThreeSymbols(string[] array1, string[] array2)
+{
+    int count = 0;
+    for (int i = 0; i < array1.Length; i++)
+    {
+    if(array1[i].Length <= 3)
+        {
+        array2[count] = array1[i];
+        count++;
+        }
+    }
+}
+void PrintArray(string[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write($"{array[i]} ");
+    }
+    Console.WriteLine();
+}
+ArrayLessEqualThreeSymbols(arrayFull, arrayNew);
+PrintArray(arrayNew);
